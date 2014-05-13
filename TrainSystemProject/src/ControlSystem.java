da54bd628886cee9de3d84144b826538f4a3f1fd
thatnerd2 <thatnerd2@gmail.com>
2014-05-13 11:57:24 -0400

    Control System bare skeleton

---
+++ TrainSystemProject/src/ControlSystem.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class ControlSystem {
+	ArrayList<Train> allTrains = new ArrayList<Train>();
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	public static void updateSystem () {
+		
+	}
+}
