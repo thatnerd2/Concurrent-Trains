09f309e13ed987b542811aca6b710bac7400a182
thatnerd2 <thatnerd2@gmail.com>
2014-05-15 11:17:42 -0400

    Initialize Control System, add a timer and a observer-pattern style update to all trains, added an updateTime method to keep track of time (measured in steps)

--- TrainSystemProject/src/ControlSystem.java
+++ TrainSystemProject/src/ControlSystem.java
@@ -1,15 +1,45 @@
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class ControlSystem {
-	ArrayList<Train> allTrains = new ArrayList<Train>();
+	static ArrayList<Train> trains;
+	static HashMap<Train, Path> paths;
+	private static int time;
 	
 	public static void main (String[] args) {
+		initialize();
 		
+		/**
+		 * Initialize, prompt, direct.
+		 */
+		
+		Timer Time = new Timer();
+		Time.scheduleAtFixedRate(new TimerTask () {
+			public void run () {
+				ControlSystem.updateSystem();
+			}
+		}, 1000, 1000);
+	}
+	
+	public static void initialize () {
+		trains = new ArrayList<Train>();
+		paths = new HashMap<Train, Path>();
 	}
 	
 	public static void updateSystem () {
+		updateTime();
 		
+		for (Train train : trains) {
+			train.update();
+		}
+	}
+	
+	public static void updateTime () {
+		time += 1;
 	}
 	
 	public static int getTime(Node nodeFrom, Node nodeTo) {
